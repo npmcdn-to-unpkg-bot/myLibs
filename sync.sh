3dc8e54a684be9a0cdc357b13dfee5c193e86af6
Zheng Ning <Kenny@Zhengs-MacBook-Pro.local>
2016-09-05 21:55:18 -0400

    Update file

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 find . -name .DS_Store -print0 | xargs -0 git rm -f --ignore-unmatch
 git add -A
-git commit -m "Add file"
+git commit -m "Update file"
 git push origin master
 git pull
